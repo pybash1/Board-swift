58d1af5991ff0b1987ece1bc753a0c7567ed96d6
PyBash <67195650+pybash1@users.noreply.github.com>
2025-09-23 22:33:20 +0530

    Initial Commit

---
+++ Board/BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardApp.swift
+//  Board
+//
+//  Created by Ananjan Mitra on 23/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
